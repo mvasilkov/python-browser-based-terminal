0bb0ddffb45107fe66afb3d71ad758b8fb62b510
Mark Vasilkov <mvasilkov@gmail.com>
2018-02-17 18:03:22 +0200

    Support resizing, better fonts

---
+++ static/debounce.js
@@ -0,0 +1,21 @@
+// https://github.com/mvasilkov/glitch2016/blob/master/couch/debounce.ts
+function debounce(proc, wait) {
+    var timeout = null
+    var lastCall
+    return function () {
+        lastCall = Date.now()
+        function wrapped() {
+            var napping = Date.now() - lastCall
+            if (napping < wait) {
+                timeout = setTimeout(wrapped, wait - napping)
+            }
+            else {
+                timeout = null
+                proc()
+            }
+        }
+        if (!timeout) {
+            timeout = setTimeout(wrapped, wait)
+        }
+    }
+}
