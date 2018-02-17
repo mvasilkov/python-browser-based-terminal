7943d4b594575ab5dd5afcf9af770e175a34aa26
Mark Vasilkov <mvasilkov@gmail.com>
2018-02-17 16:51:30 +0200

    Shut down when the underlying program exits

--- app.py
+++ app.py
@@ -14,19 +14,28 @@ from terminado import TermSocket, SingleTermManager
 OUR_ROOT = Path(__file__).parent.resolve()
 
 
+class OneManager(SingleTermManager):
+    def on_eof(self, ptywclients):
+        super().on_eof(ptywclients)
+
+        IOLoop.current().stop()
+
+
 def run(args):
     os.chdir(args.path)
 
-    term_manager = SingleTermManager(shell_command=['notes', args.path])
+    term_manager = OneManager(shell_command=['notes', args.path])
     handlers = [
         ('/socket', TermSocket, { 'term_manager': term_manager }),
         ('/()', web.StaticFileHandler, { 'path': (OUR_ROOT / 'static/index.html').as_posix() }),
         ('/static/(.*)', web.StaticFileHandler, { 'path': (OUR_ROOT / 'static').as_posix() }),
     ]  # yapf: disable
+
     app = web.Application(handlers)
     app.listen(args.port)
     print(f'Python browser-based terminal app listening on 127.0.0.1:{args.port}')
     IOLoop.current().start()
+    IOLoop.current().close()
 
 
 if __name__ == '__main__':
